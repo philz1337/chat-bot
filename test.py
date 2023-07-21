1dd368f6bdb31c6e5df055841400b833c7cd5a16
p <pberben@gmx.de>
2023-07-21 08:54:23 +0200

    x

--- test.py
+++ test.py
@@ -1,27 +1,23 @@
-from transformers import GPTNeoXForCausalLM, AutoTokenizer
+from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
 
-# model = GPTNeoXForCausalLM.from_pretrained(
-#   "EleutherAI/pythia-70m-deduped",
-#   revision="step3000",
-#   cache_dir="./pythia-70m-deduped/step3000",
-# )
+model = GPTNeoXForCausalLM.from_pretrained("PygmalionAI/pygmalion-1.3b")
+tokenizer = GPTNeoXTokenizerFast.from_pretrained("PygmalionAI/pygmalion-1.3b")
 
-# tokenizer = AutoTokenizer.from_pretrained(
-#   "EleutherAI/pythia-70m-deduped",
-#   revision="step3000",
-#   cache_dir="./pythia-70m-deduped/step3000",
-# )
+prompt = """[Isabella]'s Persona: [I am 23 year old Isabella from Darmstadt in Germany. I am naughty and want sex all the time and love penis]
 
-model = GPTNeoXForCausalLM.from_pretrained(
-  "PygmalionAI/pygmalion-1.3b",
-  cache_dir="./pygmalion-1.3b",
+Isabella: Hi i am Isabella, how can i help you?
+You: What are you doing now?
+Isabella:
+"""
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+gen_tokens = model.generate(
+    input_ids,
+    do_sample=True,
+    temperature=0.9,
+    max_length=200,
 )
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
 
-tokenizer = AutoTokenizer.from_pretrained(
-  "PygmalionAI/pygmalion-1.3b",
-  cache_dir="./pygmalion-1.3b",
-)
-
-inputs = tokenizer("Hello, I am", return_tensors="pt")
-tokens = model.generate(**inputs)
-tokenizer.decode(tokens[0])
+print("gen_text: ", gen_text)
